0959828405e5f8013cf9df5d99ed4e3cb11082d5
Raimund Hübel <raimund.huebel@hagedorn-software.de>
2020-09-01 14:22:43 +0200

    Highlighter-Tool erste implementierung.

--- src/highlight.nim
+++ src/highlight.nim
@@ -3,52 +3,204 @@
 
 import highlightpkg/colorize
 import strutils
+import re
+import os
+import parseopt
+import tables
+import algorithm
+
 
 when isMainModule:
-    echo "Foreground:"
-    echo "  - " & "black".black & " #"
-    echo "  - " & "red".red & " #"
-    echo "  - " & "green".green & " #"
-    echo "  - " & "yellow".yellow & " #"
-    echo "  - " & "blue".blue & " #"
-    echo "  - " & "magenta".magenta & " #"
-    echo "  - " & "cyan".cyan & " #"
-    echo "  - " & "white".white & " #"
 
-    echo "Background:"
-    echo "  - " & "black".onBlack & " #"
-    echo "  - " & "red".onRed & " #"
-    echo "  - " & "green".onGreen & " #"
-    echo "  - " & "yellow".onYellow & " #"
-    echo "  - " & "blue".onBlue & " #"
-    echo "  - " & "magenta".onMagenta & " #"
-    echo "  - " & "cyan".onCyan & " #"
-    echo "  - " & "white".onWhite & " #"
+    type HighlightConfigEntry = ref object of RootObj
+        regexp:      Regex
+        colorFgProc: proc (colStr: ColorizedString): ColorizedString
+        colorBgProc: proc (colStr: ColorizedString): ColorizedString
 
-    echo "Formatting:"
-    echo "  - " & "reset".yellow.onBlue.reset & " #"
-    echo "  - " & "bold".bold & " #"
-    echo "  - " & "underlined".underlined & " #"
-    echo "  - " & "reversed".reversed & " #"
+    type HighlightCommand = ref object of RootObj
+        inputFile: File
+        configEntries: seq[HighlightConfigEntry]
 
-    echo "Mixed:"
-    echo "  - " & "yellow on red + bold + underlined".yellow.onRed.bold.underlined & " #"
-    echo "  - " & "yellow on red + bold + underlined + reversed".yellow.onRed.bold.underlined.reversed & " #"
 
-    echo "Foreground-Colors (extended):"
-    for i in 0..16:
-        for j in 0..16:
-            let colorCode: int    = i * 16 + j
-            let spaces:   string  = " ".repeat(4-($colorCode).len)
-            let colorStr: string  = $(spaces & $colorCode).color(colorCode)
-            stdout.write colorStr
-        stdout.write "\n"
+    const fgColorStr2Proc = {
+        "black":   proc (colStr: ColorizedString): ColorizedString = colStr.black(),
+        "red":     proc (colStr: ColorizedString): ColorizedString = colStr.red(),
+        "green":   proc (colStr: ColorizedString): ColorizedString = colStr.green(),
+        "yellow":  proc (colStr: ColorizedString): ColorizedString = colStr.yellow(),
+        "blue":    proc (colStr: ColorizedString): ColorizedString = colStr.blue(),
+        "magenta": proc (colStr: ColorizedString): ColorizedString = colStr.magenta(),
+        "cyan":    proc (colStr: ColorizedString): ColorizedString = colStr.cyan(),
+        "white":   proc (colStr: ColorizedString): ColorizedString = colStr.white()
+    }.toTable()
 
-    echo "Background-Colors (extended):"
-    for i in 0..16:
-        for j in 0..16:
-            let colorCode: int    = i * 16 + j
-            let spaces:   string  = " ".repeat(4-($colorCode).len)
-            let colorStr: string  = $(spaces & $colorCode).onColor(colorCode)
-            stdout.write colorStr
-        stdout.write "\n"
+    const bgColorStr2Proc = {
+        "black":   proc (colStr: ColorizedString): ColorizedString = colStr.onBlack(),
+        "red":     proc (colStr: ColorizedString): ColorizedString = colStr.onRed(),
+        "green":   proc (colStr: ColorizedString): ColorizedString = colStr.onGreen(),
+        "yellow":  proc (colStr: ColorizedString): ColorizedString = colStr.onYellow(),
+        "blue":    proc (colStr: ColorizedString): ColorizedString = colStr.onBlue(),
+        "magenta": proc (colStr: ColorizedString): ColorizedString = colStr.onMagenta(),
+        "cyan":    proc (colStr: ColorizedString): ColorizedString = colStr.onCyan(),
+        "white":   proc (colStr: ColorizedString): ColorizedString = colStr.onWhite()
+    }.toTable()
+
+
+    proc newHighlightCommandFromCliArgs(args: seq[TaintedString]): HighlightCommand =
+        result = HighlightCommand()
+        result.inputFile = stdin
+        var optParser = initOptParser(args)
+        for optKind, optKey, optVal in optParser.getopt():
+            case optKind:
+            of cmdShortOption, cmdLongOption:
+                if (optKey == "e" or optKey == "entry"):
+                    let configParts = optVal.split(':', 3)
+                    let configEntry = HighlightConfigEntry()
+                    if configParts.len < 2:
+                        assert(false)
+                    configEntry.regexp = re(configParts[0])
+                    if configParts.len > 1 and configParts[1].strip != "":
+                        configEntry.colorFgProc = fgColorStr2Proc[configParts[1]]
+                    if configParts.len > 2 and configParts[2].strip != "":
+                        configEntry.colorBgProc = bgColorStr2Proc[configParts[2]]
+                    result.configEntries.add(configEntry)
+            of cmdArgument:
+                assert(false)
+            of cmdEnd:
+                assert(false)
+        return result
+
+
+    let highlightCommand = newHighlightCommandFromCliArgs(os.commandLineParams())
+    #echo $highlightCommand.inputFile.getFileHandle()
+    #for configEntry in highlightCommand.configEntries:
+    #    echo configEntry.regexp, ": fg=", configEntry.colorFg, ", bg=", configEntry.colorBg
+
+
+    for line in highlightCommand.inputFile.lines():
+        type LineConfig = tuple[
+            first:  int,
+            last:   int,
+            fgProc: proc (colStr: ColorizedString): ColorizedString,
+            bgProc: proc (colStr: ColorizedString): ColorizedString
+        ]
+
+        var gLineConfigs: seq[LineConfig] = @[]
+
+        var lineStart = 0
+        var newLine = ""
+        var restLine = line
+        while true:
+
+            var lineConfigs: seq[LineConfig] = @[]
+
+            # Get line Configs for Rest of Line ...
+            for configEntry in highlightCommand.configEntries:
+                let bounds = line.findBounds(configEntry.regexp, start=lineStart)
+                if bounds.first == -1:
+                    continue
+                let lineConfig = (
+                    first:  bounds.first,
+                    last:   bounds.last,
+                    fgProc: configEntry.colorFgProc,
+                    bgProc: configEntry.colorBgProc
+                )
+                lineConfigs.add(lineConfig)
+
+            #echo lineConfigs.len
+
+            # Wenn es nichts anzuwenden gibt, dann den rest vollständig übernehmen ...
+            if lineConfigs.len < 1:
+                break
+
+            # Wenn es genau ein Treffer gibt, dann dann diesen normal übernehmen ...
+            if lineConfigs.len == 1:
+                gLineConfigs.add(lineConfigs[0])
+                break
+
+            # Sortiere Line-Configs nach ersten treffer ...
+            lineConfigs.sort(proc (a, b: LineConfig): int =
+                if a.first < b.first:
+                    return -1
+                if a.first > b.first:
+                    return 1
+                return 0
+            )
+
+            # Wenn es mehrere Treffer gibt, dann den ersten und zweiten Treffer mergen, falls notwendig ...
+            if lineConfigs[0].last < lineConfigs[1].first:
+                gLineConfigs.add(lineConfigs[0])
+            else: # if lineConfigs[0].last >= lineConfigs[1].first
+                gLineConfigs.add((
+                    first:  lineConfigs[0].first,
+                    last:   lineConfigs[1].first - 1,
+                    fgProc: lineConfigs[0].fgProc,
+                    bgProc: lineConfigs[0].bgProc
+                ))
+                if lineConfigs[0].last < lineConfigs[1].last:
+                    gLineConfigs.add((
+                        first:  lineConfigs[1].first,
+                        last:   lineConfigs[0].last,
+                        fgProc: lineConfigs[1].fgProc,
+                        bgProc: lineConfigs[1].bgProc
+                    ))
+                if lineConfigs[0].last < lineConfigs[1].last:
+                    gLineConfigs.add((
+                        first:  lineConfigs[0].last + 1,
+                        last:   lineConfigs[1].last,
+                        fgProc: lineConfigs[0].fgProc,
+                        bgProc: lineConfigs[0].bgProc
+                    ))
+
+            let lastLineConfig = gLineConfigs[gLineConfigs.len - 1]
+            lineStart = lastLineConfig.last + 1
+
+#            # Formatiere nach ersten Treffer ...
+#            let lineConfig = lineConfigs[0]
+#
+#            # Formatiere nach ersten Treffer ...
+#            var before  = restLine[0..<lineConfig.first]
+#            var colWord = newColorizedString(restLine[lineConfig.first..lineConfig.last])
+#            if lineConfig.fgProc != nil:
+#                colWord = lineConfig.fgProc(colWord)
+#            if lineConfig.bgProc != nil:
+#                colWord = lineConfig.bgProc(colWord)
+#            restLine = restLine[lineConfig.last+1..<restLine.len]
+#            newLine = newLine & before
+#            newLine = newLine & $colWord
+
+            #let bounds = restLine.findBounds(highlightCommand.configEntries[0].regexp)
+            #let fgColProc = highlightCommand.configEntries[0].colorFgProc
+            #let bgColProc = highlightCommand.configEntries[0].colorBgProc
+            #if bounds.first != -1:
+            #    var before  = restLine[0..<bounds.first]
+            #    var colWord = newColorizedString(restLine[bounds.first..bounds.last])
+            #    if fgColProc != nil:
+            #        colWord = fgColProc(colWord)
+            #    if bgColProc != nil:
+            #        colWord = bgColProc(colWord)
+            #    restLine   = restLine[bounds.last+1..<restLine.len]
+            #    newLine = newLine & before
+            #    newLine = newLine & $colWord
+            #else:
+            #    newLine = newLine & restLine
+            #    break
+        #echo $gLineConfigs
+
+        if gLineConfigs.len == 0:
+            stdout.write line
+        else:
+            var lineStart = 0
+            for idx in 0 .. gLineConfigs.len-1:
+                let lineConfig = gLineConfigs[idx]
+                if lineStart < lineConfig.first:
+                    stdout.write line[lineStart .. lineConfig.first-1]
+                var colWord = newColorizedString(line[lineConfig.first .. lineConfig.last])
+                if lineConfig.fgProc != nil:
+                    colWord = lineConfig.fgProc(colWord)
+                if lineConfig.bgProc != nil:
+                    colWord = lineConfig.bgProc(colWord)
+                lineStart = lineConfig.last + 1
+                stdout.write $colWord
+            if lineStart < line.len:
+                stdout.write line[lineStart .. line.len-1]
+        stdout.write("\n")
--- src/highlightpkg/colorize.nim
+++ src/highlightpkg/colorize.nim
@@ -11,161 +11,167 @@ import strutils
 
 
 
-type ColorizedString* = ref object of RootObj
-    str:          string
-    fgColor:      int
-    bgColor:      int
-    cdBold:       int
-    cdUnderlined: int
-    cdReversed:   int
+when defined(Posix):
+
+
+    type ColorizedString* = ref object of RootObj
+        str:          string
+        fgColor:      int
+        bgColor:      int
+        cdBold:       int
+        cdUnderlined: int
+        cdReversed:   int
 
 
 
-proc newColorizedString*(str: string): ColorizedString =
-    return ColorizedString(str: str)
+    proc newColorizedString*(str: string): ColorizedString =
+        return ColorizedString(str: str)
 
 
 
-proc reset*(colStr: ColorizedString): ColorizedString =
-    colStr.fgColor      = 0
-    colStr.bgColor      = 0
-    colStr.cdBold       = 0
-    colStr.cdUnderlined = 0
-    colStr.cdReversed   = 0
-    return colStr
-
-
-
-
-proc color*(colStr: ColorizedString, color: int): ColorizedString =
-    colStr.fgColor = 1000 + color
-    return colStr
-
-proc black*(colStr: ColorizedString): ColorizedString =
-    colStr.fgColor = 30
-    return colStr
-
-proc red*(colStr: ColorizedString): ColorizedString =
-    colStr.fgColor = 31
-    return colStr
-
-proc green*(colStr: ColorizedString): ColorizedString =
-    colStr.fgColor = 32
-    return colStr
-
-proc yellow*(colStr: ColorizedString): ColorizedString =
-    colStr.fgColor = 33
-    return colStr
-
-proc blue*(colStr: ColorizedString): ColorizedString =
-    colStr.fgColor = 34
-    return colStr
-
-proc magenta*(colStr: ColorizedString): ColorizedString =
-    colStr.fgColor = 35
-    return colStr
-
-proc cyan*(colStr: ColorizedString): ColorizedString =
-    colStr.fgColor = 36
-    return colStr
-
-proc white*(colStr: ColorizedString): ColorizedString =
-    colStr.fgColor = 37
-    return colStr
+    proc reset*(colStr: ColorizedString): ColorizedString =
+        colStr.fgColor      = 0
+        colStr.bgColor      = 0
+        colStr.cdBold       = 0
+        colStr.cdUnderlined = 0
+        colStr.cdReversed   = 0
+        return colStr
 
 
 
 
-#proc onDefault*(colStr: ColorizedString): ColorizedString =
-#    colStr.bgColor = 0
-#    return colStr
+    proc color*(colStr: ColorizedString, color: int): ColorizedString =
+        colStr.fgColor = 1000 + color
+        return colStr
 
-proc onColor*(colStr: ColorizedString, color: int): ColorizedString =
-    colStr.bgColor = 1000 + color
-    return colStr
+    proc black*(colStr: ColorizedString): ColorizedString =
+        colStr.fgColor = 30
+        return colStr
 
-proc onBlack*(colStr: ColorizedString): ColorizedString =
-    colStr.bgColor = 40
-    return colStr
+    proc red*(colStr: ColorizedString): ColorizedString =
+        colStr.fgColor = 31
+        return colStr
 
-proc onRed*(colStr: ColorizedString): ColorizedString =
-    colStr.bgColor = 41
-    return colStr
+    proc green*(colStr: ColorizedString): ColorizedString =
+        colStr.fgColor = 32
+        return colStr
 
-proc onGreen*(colStr: ColorizedString): ColorizedString =
-    colStr.bgColor = 42
-    return colStr
+    proc yellow*(colStr: ColorizedString): ColorizedString =
+        colStr.fgColor = 33
+        return colStr
 
-proc onYellow*(colStr: ColorizedString): ColorizedString =
-    colStr.bgColor = 43
-    return colStr
+    proc blue*(colStr: ColorizedString): ColorizedString =
+        colStr.fgColor = 34
+        return colStr
 
-proc onBlue*(colStr: ColorizedString): ColorizedString =
-    colStr.bgColor = 44
-    return colStr
+    proc magenta*(colStr: ColorizedString): ColorizedString =
+        colStr.fgColor = 35
+        return colStr
 
-proc onMagenta*(colStr: ColorizedString): ColorizedString =
-    colStr.bgColor = 45
-    return colStr
+    proc cyan*(colStr: ColorizedString): ColorizedString =
+        colStr.fgColor = 36
+        return colStr
 
-proc onCyan*(colStr: ColorizedString): ColorizedString =
-    colStr.bgColor = 46
-    return colStr
-
-proc onWhite*(colStr: ColorizedString): ColorizedString =
-    colStr.bgColor = 47
-    return colStr
+    proc white*(colStr: ColorizedString): ColorizedString =
+        colStr.fgColor = 37
+        return colStr
 
 
 
 
-proc bold*(colStr: ColorizedString): ColorizedString =
-    colStr.cdBold = 1
-    return colStr
+    #proc onDefault*(colStr: ColorizedString): ColorizedString =
+    #    colStr.bgColor = 0
+    #    return colStr
 
-proc underlined*(colStr: ColorizedString): ColorizedString =
-    colStr.cdUnderlined = 4
-    return colStr
+    proc onColor*(colStr: ColorizedString, color: int): ColorizedString =
+        colStr.bgColor = 1000 + color
+        return colStr
 
-proc reversed*(colStr: ColorizedString): ColorizedString =
-    colStr.cdReversed = 7
-    return colStr
+    proc onBlack*(colStr: ColorizedString): ColorizedString =
+        colStr.bgColor = 40
+        return colStr
+
+    proc onRed*(colStr: ColorizedString): ColorizedString =
+        colStr.bgColor = 41
+        return colStr
+
+    proc onGreen*(colStr: ColorizedString): ColorizedString =
+        colStr.bgColor = 42
+        return colStr
+
+    proc onYellow*(colStr: ColorizedString): ColorizedString =
+        colStr.bgColor = 43
+        return colStr
+
+    proc onBlue*(colStr: ColorizedString): ColorizedString =
+        colStr.bgColor = 44
+        return colStr
+
+    proc onMagenta*(colStr: ColorizedString): ColorizedString =
+        colStr.bgColor = 45
+        return colStr
+
+    proc onCyan*(colStr: ColorizedString): ColorizedString =
+        colStr.bgColor = 46
+        return colStr
+
+    proc onWhite*(colStr: ColorizedString): ColorizedString =
+        colStr.bgColor = 47
+        return colStr
 
 
 
 
-proc `$`*(colStr: ColorizedString): string =
-    var formatStrs: seq[string] = @[]
-    if colStr.fgColor != 0:
-        if colStr.fgColor < 1000:
-            formatStrs.add $colStr.fgColor
+    proc bold*(colStr: ColorizedString): ColorizedString =
+        colStr.cdBold = 1
+        return colStr
+
+    proc underlined*(colStr: ColorizedString): ColorizedString =
+        colStr.cdUnderlined = 4
+        return colStr
+
+    proc reversed*(colStr: ColorizedString): ColorizedString =
+        colStr.cdReversed = 7
+        return colStr
+
+
+
+
+    proc `$`*(colStr: ColorizedString): string =
+        var formatStrs: seq[string] = @[]
+        if colStr.fgColor != 0:
+            if colStr.fgColor < 1000:
+                formatStrs.add $colStr.fgColor
+            else:
+                formatStrs.add "38;5;" & $(colStr.fgColor - 1000)
+        if colStr.bgColor != 0:
+            if colStr.bgColor < 1000:
+                formatStrs.add $colStr.bgColor
+            else:
+                formatStrs.add "48;5;" & $(colStr.bgColor - 1000)
+        if colStr.cdBold != 0:
+            formatStrs.add $colStr.cdBold
+        if colStr.cdUnderlined != 0:
+            formatStrs.add $colStr.cdUnderlined
+        if colStr.cdReversed != 0:
+            formatStrs.add $colStr.cdReversed
+        if formatStrs.len != 0:
+            return  "\x1B[0;" & formatStrs.join(";") & "m" & colStr.str & "\x1B[0m"
         else:
-            formatStrs.add "38;5;" & $(colStr.fgColor - 1000)
-    if colStr.bgColor != 0:
-        if colStr.bgColor < 1000:
-            formatStrs.add $colStr.bgColor
-        else:
-            formatStrs.add "48;5;" & $(colStr.bgColor - 1000)
-    if colStr.cdBold != 0:
-        formatStrs.add $colStr.cdBold
-    if colStr.cdUnderlined != 0:
-        formatStrs.add $colStr.cdUnderlined
-    if colStr.cdReversed != 0:
-        formatStrs.add $colStr.cdReversed
-    if formatStrs.len != 0:
-        return  "\x1B[0;" & formatStrs.join(";") & "m" & colStr.str & "\x1B[0m"
-    else:
-        return colStr.str
+            return colStr.str
 
 
-proc `&`*(a: string, b: ColorizedString): string =
-    return a & $b
+    proc `&`*(a: string, b: ColorizedString): string =
+        return a & $b
 
-proc `&`*(a: ColorizedString, b: string): string =
-    return $a & b
+    proc `&`*(a: ColorizedString, b: string): string =
+        return $a & b
 
-proc `&`*(a: ColorizedString, b: ColorizedString): string =
-    return $a & $b
+    proc `&`*(a: ColorizedString, b: ColorizedString): string =
+        return $a & $b
+
+
+
 
 
 proc color*(str: string, color: int): ColorizedString =
