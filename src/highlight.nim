b5865a43f5a4c551f93ccf08b67c893fceb6bfb0
Raimund Hübel <raimund.huebel@hagedorn-software.de>
2020-09-08 17:43:31 +0200

    Highlighter first draft finished.

--- src/highlight.nim
+++ src/highlight.nim
@@ -8,41 +8,30 @@ import os
 import parseopt
 import tables
 import algorithm
+import heapqueue
 
 
 when isMainModule:
 
     type HighlightConfigEntry = ref object of RootObj
-        regexp:      Regex
-        colorFgProc: proc (colStr: ColorizedString): ColorizedString
-        colorBgProc: proc (colStr: ColorizedString): ColorizedString
+        regexp:  Regex
+        colorFg: string
+        colorBg: string
+        #colorFgProc: proc (colStr: ColorizedString): ColorizedString
+        #colorBgProc: proc (colStr: ColorizedString): ColorizedString
 
     type HighlightCommand = ref object of RootObj
         inputFile: File
+        isHelp:    bool
         configEntries: seq[HighlightConfigEntry]
 
+    type Breakpoint = tuple
+        pos:     int
+        prio:    int
+        isReset: bool
+        colorFg: string
+        colorBg: string
 
-    const fgColorStr2Proc = {
-        "black":   proc (colStr: ColorizedString): ColorizedString = colStr.black(),
-        "red":     proc (colStr: ColorizedString): ColorizedString = colStr.red(),
-        "green":   proc (colStr: ColorizedString): ColorizedString = colStr.green(),
-        "yellow":  proc (colStr: ColorizedString): ColorizedString = colStr.yellow(),
-        "blue":    proc (colStr: ColorizedString): ColorizedString = colStr.blue(),
-        "magenta": proc (colStr: ColorizedString): ColorizedString = colStr.magenta(),
-        "cyan":    proc (colStr: ColorizedString): ColorizedString = colStr.cyan(),
-        "white":   proc (colStr: ColorizedString): ColorizedString = colStr.white()
-    }.toTable()
-
-    const bgColorStr2Proc = {
-        "black":   proc (colStr: ColorizedString): ColorizedString = colStr.onBlack(),
-        "red":     proc (colStr: ColorizedString): ColorizedString = colStr.onRed(),
-        "green":   proc (colStr: ColorizedString): ColorizedString = colStr.onGreen(),
-        "yellow":  proc (colStr: ColorizedString): ColorizedString = colStr.onYellow(),
-        "blue":    proc (colStr: ColorizedString): ColorizedString = colStr.onBlue(),
-        "magenta": proc (colStr: ColorizedString): ColorizedString = colStr.onMagenta(),
-        "cyan":    proc (colStr: ColorizedString): ColorizedString = colStr.onCyan(),
-        "white":   proc (colStr: ColorizedString): ColorizedString = colStr.onWhite()
-    }.toTable()
 
 
     proc newHighlightCommandFromCliArgs(args: seq[TaintedString]): HighlightCommand =
@@ -52,17 +41,23 @@ when isMainModule:
         for optKind, optKey, optVal in optParser.getopt():
             case optKind:
             of cmdShortOption, cmdLongOption:
-                if (optKey == "e" or optKey == "entry"):
+                if (optKey == "h" or optKey == "help"):
+                    result.isHelp = true
+                elif (optKey == "e" or optKey == "entry"):
                     let configParts = optVal.split(':', 3)
                     let configEntry = HighlightConfigEntry()
                     if configParts.len < 2:
                         assert(false)
                     configEntry.regexp = re(configParts[0])
                     if configParts.len > 1 and configParts[1].strip != "":
-                        configEntry.colorFgProc = fgColorStr2Proc[configParts[1]]
+                        configEntry.colorFg = configParts[1]
+                        #configEntry.colorFgProc = fgColorStr2Proc[configParts[1]]
                     if configParts.len > 2 and configParts[2].strip != "":
-                        configEntry.colorBgProc = bgColorStr2Proc[configParts[2]]
+                        configEntry.colorBg = configParts[2]
+                        #configEntry.colorBgProc = bgColorStr2Proc[configParts[2]]
                     result.configEntries.add(configEntry)
+                else:
+                    assert(false)
             of cmdArgument:
                 assert(false)
             of cmdEnd:
@@ -71,136 +66,182 @@ when isMainModule:
 
 
     let highlightCommand = newHighlightCommandFromCliArgs(os.commandLineParams())
-    #echo $highlightCommand.inputFile.getFileHandle()
-    #for configEntry in highlightCommand.configEntries:
-    #    echo configEntry.regexp, ": fg=", configEntry.colorFg, ", bg=", configEntry.colorBg
+
+
+    if highlightCommand.isHelp:
+        echo "Usage: " & getAppFilename() & " [OPTIONS]"
+        echo ""
+        echo "Options:"
+        echo "  -h | --help         Print this help"
+        echo "  -e= | --entry=      Adds a highlight-Entry of the form 'REGEX:FgColor:BgColor' (see section Colors)"
+        echo ""
+        echo "Colors:"
+        echo "  black | white | red | green | blue | yellow | cyan | magenta"
+        echo ""
+        echo "Example:"
+        echo "  $ cat aLogFile.log | highligter -e='^E.*:red' -e='^W.*:yellow -e'^I.*:white' -e='^D.*|^T.*:black' -e='SampleService:blue'"
+        system.quit(0)
 
 
     for line in highlightCommand.inputFile.lines():
-        type LineConfig = tuple[
-            first:  int,
-            last:   int,
-            fgProc: proc (colStr: ColorizedString): ColorizedString,
-            bgProc: proc (colStr: ColorizedString): ColorizedString
-        ]
 
-        var gLineConfigs: seq[LineConfig] = @[]
+        var breakpoints: seq[Breakpoint] = @[]
 
-        var lineStart = 0
-        var newLine = ""
-        var restLine = line
-        while true:
-
-            var lineConfigs: seq[LineConfig] = @[]
-
-            # Get line Configs for Rest of Line ...
-            for configEntry in highlightCommand.configEntries:
+        # Get all Breakpoints of all Highlight-Configs ...
+        for idxCe in 0 ..< highlightCommand.configEntries.len:
+            let configEntry = highlightCommand.configEntries[idxCe]
+            var lineStart = 0
+            while true:
                 let bounds = line.findBounds(configEntry.regexp, start=lineStart)
                 if bounds.first == -1:
-                    continue
-                let lineConfig = (
-                    first:  bounds.first,
-                    last:   bounds.last,
-                    fgProc: configEntry.colorFgProc,
-                    bgProc: configEntry.colorBgProc
-                )
-                lineConfigs.add(lineConfig)
-
-            #echo lineConfigs.len
-
-            # Wenn es nichts anzuwenden gibt, dann den rest vollständig übernehmen ...
-            if lineConfigs.len < 1:
-                break
-
-            # Wenn es genau ein Treffer gibt, dann dann diesen normal übernehmen ...
-            if lineConfigs.len == 1:
-                gLineConfigs.add(lineConfigs[0])
-                break
-
-            # Sortiere Line-Configs nach ersten treffer ...
-            lineConfigs.sort(proc (a, b: LineConfig): int =
-                if a.first < b.first:
-                    return -1
-                if a.first > b.first:
-                    return 1
-                return 0
-            )
-
-            # Wenn es mehrere Treffer gibt, dann den ersten und zweiten Treffer mergen, falls notwendig ...
-            if lineConfigs[0].last < lineConfigs[1].first:
-                gLineConfigs.add(lineConfigs[0])
-            else: # if lineConfigs[0].last >= lineConfigs[1].first
-                gLineConfigs.add((
-                    first:  lineConfigs[0].first,
-                    last:   lineConfigs[1].first - 1,
-                    fgProc: lineConfigs[0].fgProc,
-                    bgProc: lineConfigs[0].bgProc
+                    break
+                lineStart = bounds.last + 1
+                breakpoints.add((
+                    pos:     bounds.first,
+                    prio:    idxCe,
+                    isReset: false,
+                    colorFg: configEntry.colorFg,
+                    colorBg: configEntry.colorBg
                 ))
-                if lineConfigs[0].last < lineConfigs[1].last:
-                    gLineConfigs.add((
-                        first:  lineConfigs[1].first,
-                        last:   lineConfigs[0].last,
-                        fgProc: lineConfigs[1].fgProc,
-                        bgProc: lineConfigs[1].bgProc
-                    ))
-                if lineConfigs[0].last < lineConfigs[1].last:
-                    gLineConfigs.add((
-                        first:  lineConfigs[0].last + 1,
-                        last:   lineConfigs[1].last,
-                        fgProc: lineConfigs[0].fgProc,
-                        bgProc: lineConfigs[0].bgProc
-                    ))
+                breakpoints.add((
+                    pos:     bounds.last + 1,
+                    prio:    idxCe,
+                    isReset: true,
+                    colorFg: configEntry.colorFg,
+                    colorBg: configEntry.colorBg
+                ))
 
-            let lastLineConfig = gLineConfigs[gLineConfigs.len - 1]
-            lineStart = lastLineConfig.last + 1
+        # Wenn es nichts anzuwenden gibt, dann die Zeile ausgeben und nächste ...
+        if breakpoints.len == 0:
+            stdout.write(line)
+            stdout.write("\n")
+            continue
 
-#            # Formatiere nach ersten Treffer ...
-#            let lineConfig = lineConfigs[0]
-#
-#            # Formatiere nach ersten Treffer ...
-#            var before  = restLine[0..<lineConfig.first]
-#            var colWord = newColorizedString(restLine[lineConfig.first..lineConfig.last])
-#            if lineConfig.fgProc != nil:
-#                colWord = lineConfig.fgProc(colWord)
-#            if lineConfig.bgProc != nil:
-#                colWord = lineConfig.bgProc(colWord)
-#            restLine = restLine[lineConfig.last+1..<restLine.len]
-#            newLine = newLine & before
-#            newLine = newLine & $colWord
+        # Sortiere Line-Configs nach Anfagspos, dann nach Endpos ...
+        breakpoints.sort(proc (a, b: Breakpoint): int =
+            if a.pos < b.pos:
+                return -1
+            if a.pos > b.pos:
+                return 1
+            if a.prio < b.prio:
+                return 1
+            if a.prio > b.prio:
+                return -1
+            return 0
+        )
 
-            #let bounds = restLine.findBounds(highlightCommand.configEntries[0].regexp)
-            #let fgColProc = highlightCommand.configEntries[0].colorFgProc
-            #let bgColProc = highlightCommand.configEntries[0].colorBgProc
-            #if bounds.first != -1:
-            #    var before  = restLine[0..<bounds.first]
-            #    var colWord = newColorizedString(restLine[bounds.first..bounds.last])
-            #    if fgColProc != nil:
-            #        colWord = fgColProc(colWord)
-            #    if bgColProc != nil:
-            #        colWord = bgColProc(colWord)
-            #    restLine   = restLine[bounds.last+1..<restLine.len]
-            #    newLine = newLine & before
-            #    newLine = newLine & $colWord
-            #else:
-            #    newLine = newLine & restLine
-            #    break
-        #echo $gLineConfigs
+        #echo "Vorher:"
+        #for breakpoint in breakpoints:
+        #    echo "  ", $breakpoint
 
-        if gLineConfigs.len == 0:
-            stdout.write line
-        else:
+
+        # Breakpoint-Liste Reset-Points aufräumen ...
+        if breakpoints.len >= 1:
+            var newBreakpoints:    seq[Breakpoint] = @[]
+            var activeBreakpoints: seq[Breakpoint] = @[]
+            for idxBrCurr in 0 ..< breakpoints.len:
+                let brCurr = breakpoints[idxBrCurr]
+                var brNew: Breakpoint
+
+                if not brCurr.isReset:
+                    ## Reguläre Breakpoints immer übernehmen ...
+                    activeBreakpoints.add( brCurr )
+                    brNew = brCurr
+
+                elif brCurr.isReset and activeBreakpoints.len == 0:
+                    ## Resetpunkt, aber kein aktiver Breakpoint vorhanden, reset übernehmen ...
+                    brNew = brCurr
+
+                elif brCurr.isReset and activeBreakpoints.len > 0:
+                    ## Resetpunkte mit aktiven Colorpoints ...
+                    let brLast = activeBreakpoints[activeBreakpoints.len-1]
+
+                    # Wenn der aktuelle reset Breakpoint NICHT mit dem letzten aktiven übereinstimmt, diesen verwerfen ...
+                    if brCurr.colorFg != brLast.colorFg or brCurr.colorBg != brLast.colorBg:
+                        continue
+
+                    # Der aktuelle reset Breakpoint stimmt dem letzten aktiven überein ...
+
+                    # Letzten aktiven Breakpoint entfernen ...
+                    activeBreakpoints.setLen(activeBreakpoints.len-1)
+
+                    if activeBreakpoints.len == 0:
+                        # Wenn kein aktiver Breakpoint vorhanden ist, den aktuellen Resetpoint übernehmen ...
+                        brNew = brCurr
+                    else:
+                        brNew = activeBreakpoints[activeBreakpoints.len-1]
+                        brNew.pos  = brCurr.pos
+                        brNew.prio = brCurr.prio
+
+                # Neuen Farbpunkt verwerfen, wenn der vorherige Farbpunkt die selbe Ausprägung hat ...
+                if not brNew.isReset and newBreakpoints.len > 0:
+                    let brPrev = newBreakpoints[newBreakpoints.len-1]
+                    if not brPrev.isReset and brPrev.colorFg == brNew.colorFg and brPrev.colorBg == brNew.colorBg:
+                        continue
+
+                # Neuen Farbpunkt verwerfen, wenn der nachfolgende Punkt auf die gleiche Stelle zeigt ...
+                if idxBrCurr < breakpoints.len-1:
+                    let brNext = breakpoints[idxBrCurr+1]
+                    if brNext.pos == brNew.pos:
+                        continue
+
+                # Neuen Punkt übernehmen ...
+                newBreakpoints.add( brNew )
+
+            breakpoints = newBreakpoints
+
+
+        #echo "Nacher:"
+        #for breakpoint in breakpoints:
+        #    echo "  ", $breakpoint
+
+        #echo "Test:"
+        #block:
+        #    var lineStart = 0
+        #    for breakpoint in breakpoints:
+        #        let linePart = line[lineStart..<breakpoint.pos]
+        #        stdout.write linePart
+        #        lineStart = breakpoint.pos
+        #        stdout.write "|"
+        #    let linePart = line[lineStart..<line.len]
+        #    stdout.write linePart
+        #    stdout.write "\n"
+
+        const colorCodesMap = {
+            # Reset
+            ":":   "\x1B[0m",
+            # Only Foreground ...
+            "black:":   "\x1B[0;30m",
+            "red:":     "\x1B[0;31m",
+            "green:":   "\x1B[0;32m",
+            "yellow:":  "\x1B[0;33m",
+            "blue:":    "\x1B[0;34m",
+            "magenta:": "\x1B[0;35m",
+            "cyan:":    "\x1B[0;36m",
+            "white:":   "\x1B[0;37m",
+            # Only Background ...
+            ":black":   "\x1B[0;40m",
+            ":red":     "\x1B[0;41m",
+            ":green":   "\x1B[0;42m",
+            ":yellow":  "\x1B[0;43m",
+            ":blue":    "\x1B[0;44m",
+            ":magenta": "\x1B[0;45m",
+            ":cyan":    "\x1B[0;46m",
+            ":white":   "\x1B[0;47m",
+        }.toTable()
+
+
+        block:
             var lineStart = 0
-            for idx in 0 .. gLineConfigs.len-1:
-                let lineConfig = gLineConfigs[idx]
-                if lineStart < lineConfig.first:
-                    stdout.write line[lineStart .. lineConfig.first-1]
-                var colWord = newColorizedString(line[lineConfig.first .. lineConfig.last])
-                if lineConfig.fgProc != nil:
-                    colWord = lineConfig.fgProc(colWord)
-                if lineConfig.bgProc != nil:
-                    colWord = lineConfig.bgProc(colWord)
-                lineStart = lineConfig.last + 1
-                stdout.write $colWord
-            if lineStart < line.len:
-                stdout.write line[lineStart .. line.len-1]
-        stdout.write("\n")
+            for breakpoint in breakpoints:
+                let linePart = line[lineStart..<breakpoint.pos]
+                stdout.write linePart
+                lineStart = breakpoint.pos
+                #stdout.write "|"
+                var colorCode: string = colorCodesMap[":"]
+                if not breakpoint.isReset:
+                    colorCode = colorCodesMap[breakpoint.colorFg & ":" & breakpoint.colorBg]
+                stdout.write(colorCode)
+            let linePart = line[lineStart..<line.len]
+            stdout.write linePart
+            stdout.write "\n"
